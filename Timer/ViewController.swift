629f71a0094ce1a579b87cd01e62897cb9548f07
JangWoojun <woojun0107@naver.com>
2023-01-21 07:23:34 +0900

    design: timeLabel 다자인 수정
    시간 없을 때 디폴트 값 설정

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -12,13 +12,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var startButton: UIButton!
     
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         resetButton.layer.borderWidth = 1
         resetButton.layer.borderColor = UIColor.black.cgColor
+        configureUI()
     }
 
-
+    func configureUI() {
+        timeLabel.text = "0s"
+    }
+    
 }
 
